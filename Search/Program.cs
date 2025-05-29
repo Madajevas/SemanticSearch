51e2acb3cb1e2528dea2a36e23a215b2efd63c29
Tomas Madajevas <tomas.madajevas@live.com>
2025-05-29 19:51:29 +0300

    create table with vector data type

--- Search/Program.cs
+++ Search/Program.cs
@@ -1,14 +1,29 @@
 ﻿
+using Dapper;
+
 using MessagePack;
 
-using Microsoft.Extensions.Hosting;
+using Microsoft.Data.SqlClient;
 
 using System.IO.Pipes;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.AddServiceDefaults();
+// docker volume create mssql_vector
+// docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=test-1234" -p 1433:1433 --user root -v mssql_vector:/var/opt/mssql/data -d mcr.microsoft.com/mssql/server:2025-latest
 
-var app = builder.Build();
+var connectionString = "Server=127.0.0.1,1433;Database=main;User Id=sa;Password=test-1234;Database=movies;MultipleActiveResultSets=True;TrustServerCertificate=true;";
+using var connection = new SqlConnection(connectionString);
+await connection.ExecuteAsync("""
+    IF  NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[movies]') AND type in (N'U'))
+    BEGIN
+    CREATE TABLE movies (
+        id INT IDENTITY (1,1),
+        name VARCHAR(500) NOT NULL,
+        description TEXT NOT NULL,
+        vector VECTOR(900) NOT NULL
+    );
+    END
+    """);
+
 
 
 using var client = new NamedPipeClientStream(".", "testpipe", PipeDirection.InOut);

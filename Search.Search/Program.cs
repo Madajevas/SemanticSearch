1ccfdbd24a2a4db814ecf4b1ed73432342c1daf5
Tomas Madajevas <tomas.madajevas@live.com>
2025-06-25 13:31:32 +0300

    figure out vector length dynamically

--- Search.Search/Program.cs
+++ Search.Search/Program.cs
@@ -8,10 +8,15 @@ using Microsoft.Data.SqlClient;
 
 using Spectre.Console;
 
-const ushort noOfDimensions = 900;
 var connectionString = "Server=127.0.0.1,1433;Database=main;User Id=sa;Password=test-1234;Database=movies;MultipleActiveResultSets=True;TrustServerCertificate=true;";
 using var connection = new SqlConnection(connectionString);
 await connection.OpenAsync();
+
+var noOfDimensions = await connection.ExecuteScalarAsync<ushort>("""
+    SELECT vector_dimensions FROM sys.columns AS c
+    INNER JOIN sys.tables AS t ON t.object_id = c.object_id
+    WHERE t.name = 'movies' and c.name = 'vector'
+    """);
 
 using var embeddingClient = new EmbeddingClient();
 
@@ -20,7 +25,7 @@ while (true)
     var phrase = AnsiConsole.Prompt(new TextPrompt<string>("Phrase to look for:"));
     var search = await embeddingClient.GetEmbeddingAsync(phrase, default);
     var query = $"""
-    SELECT TOP (10) name, description, VECTOR_DISTANCE('cosine', CAST(JSON_ARRAY({string.Join(',', search)}) AS VECTOR({noOfDimensions})), [vector]) AS distance
+    SELECT TOP (20) name, description, VECTOR_DISTANCE('cosine', CAST(JSON_ARRAY({string.Join(',', search)}) AS VECTOR({noOfDimensions})), [vector]) AS distance
     FROM movies
     ORDER BY distance
     """;
--- Search/Program.cs
+++ Search/Program.cs
@@ -8,27 +8,17 @@ using EmbeddingsService.Client;
 
 using Microsoft.Data.SqlClient;
 
+using System.Data.Common;
 using System.Diagnostics;
 using System.Globalization;
 
 // docker volume create mssql_vector
 // docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=test-1234" -p 1433:1433 --user root -v mssql_vector:/var/opt/mssql/data -d mcr.microsoft.com/mssql/server:2025-latest
 
-const ushort noOfDimensions = 900;
+bool isTableCreated = false;
 var connectionString = "Server=127.0.0.1,1433;Database=main;User Id=sa;Password=test-1234;Database=movies;MultipleActiveResultSets=True;TrustServerCertificate=true;";
 using var connection = new SqlConnection(connectionString);
 await connection.OpenAsync();
-await connection.ExecuteAsync($"""
-    IF  NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[movies]') AND type in (N'U'))
-    BEGIN
-    CREATE TABLE movies (
-        id INT IDENTITY (1,1),
-        name VARCHAR(500) NOT NULL,
-        description TEXT NOT NULL,
-        vector VECTOR({noOfDimensions}) NOT NULL
-    );
-    END
-    """);
 
 
 var sw = new Stopwatch();
@@ -50,10 +40,11 @@ using (var csv = new CsvReader(moviesFileStream, new CsvConfiguration(CultureInf
         {
             using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
             var embedding = await embeddingClient.GetEmbeddingAsync(movie.Description, cts.Token);
+            await EnsureMoviesTable(connection, transaction, (ushort)embedding.Length);
 
             var sql = $"""
             INSERT INTO movies (name, description, vector)
-            VALUES (@Title, @Description, CAST(JSON_ARRAY({string.Join(',', embedding)}) AS VECTOR({noOfDimensions})))
+            VALUES (@Title, @Description, CAST(JSON_ARRAY({string.Join(',', embedding)}) AS VECTOR({embedding.Length})))
             """;
             await connection.ExecuteAsync(sql, movie, transaction);
 
@@ -67,6 +58,27 @@ using (var csv = new CsvReader(moviesFileStream, new CsvConfiguration(CultureInf
 
 Console.WriteLine("Done! Press any key to exit.");
 Console.ReadKey();
+
+async ValueTask EnsureMoviesTable(SqlConnection connection, DbTransaction transaction, ushort vectorLength)
+{
+    if (isTableCreated)
+    {
+        return;
+    }
+
+    await connection.ExecuteAsync($"""
+        IF  NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[movies]') AND type in (N'U'))
+        BEGIN
+        CREATE TABLE movies (
+            id INT IDENTITY (1,1),
+            name VARCHAR(500) NOT NULL,
+            description TEXT NOT NULL,
+            vector VECTOR({vectorLength}) NOT NULL
+        );
+        END
+        """, transaction: transaction);
+    isTableCreated = true;
+}
 
 record Movie
 {

3ee6d41357888ab659834c7f1af85c415e2e2370
Tomas Madajevas <tomas.madajevas@live.com>
2025-06-25 13:32:59 +0300

    change model again to increase accuracy

--- EmbeddingsService/TextEmbedder.cs
+++ EmbeddingsService/TextEmbedder.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Hosting;
-using Microsoft.ML;
+﻿using FastBertTokenizer;
+
+using Microsoft.Extensions.Hosting;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
 
 using System.Buffers;
 using System.IO.Pipes;
@@ -8,30 +11,21 @@ namespace EmbeddingsService
 {
     internal class TextEmbedder : BackgroundService
     {
-        private PredictionEngine<TextData, TransformedTextData> predictionEngine;
+        private InferenceSession session = null!;
+        private BertTokenizer tokenizer = null!;
+
+        public override void Dispose()
+        {
+            session?.Dispose();
+            base.Dispose();
+        }
 
         public override Task StartAsync(CancellationToken cancellationToken)
         {
-            var mlContext = new MLContext();
-            var emptyDataView = mlContext.Data.LoadFromEnumerable(Array.Empty<TextData>());
-
-            var textPipeline = mlContext.Transforms.Text.NormalizeText(nameof(TextData.Text))
-                .Append(mlContext.Transforms.Text.TokenizeIntoWords(outputColumnName: "Tokens", inputColumnName: nameof(TextData.Text)))
-                // .Append(mlContext.Transforms.Text.ApplyWordEmbedding(outputColumnName: nameof(TransformedTextData.Vector), inputColumnName: "Tokens"))
-                .Append(mlContext.Transforms.Text.ApplyWordEmbedding(
-                   outputColumnName: nameof(TransformedTextData.Vector),
-                   inputColumnName: "Tokens",
-
-                   // Invoke-RestMethod -Uri https://mlpublicassets.blob.core.windows.net/assets/wordvectors/wiki.en.vec -OutFile C:/temp/wiki.en.vec
-                   // customModelFile: @"C:\projects\wiki.en.vec"
-
-                   // Invoke-RestMethod -Uri https://nlp.stanford.edu/data/glove.840B.300d.zip -OutFile C:/temp/glove.840B.300d.zip
-                   // Expand-Archive -Path C:/temp/glove.840B.300d.zip -DestinationPath C:/temp/glove.840B.300d
-                   customModelFile: @"C:\projects\glove.840B.300d.txt"
-                ));
-
-            var textTransformer = textPipeline.Fit(emptyDataView);
-            predictionEngine = mlContext.Model.CreatePredictionEngine<TextData, TransformedTextData>(textTransformer);
+            session = new InferenceSession(@"C:\projects\e5-large-v2\onnx\model.onnx");
+            tokenizer = new BertTokenizer();
+            using var tokenJsonStream = File.OpenRead(@"C:\projects\e5-large-v2\tokenizer.json");
+            tokenizer.LoadTokenizerJson(tokenJsonStream);
 
             return base.StartAsync(cancellationToken);
         }
@@ -57,13 +51,12 @@ namespace EmbeddingsService
                         var text = System.Text.Encoding.UTF8.GetString(payloadBuffer, 0, length);
                         ArrayPool<byte>.Shared.Return(payloadBuffer);
 
-                        var data = new TextData { Text = text };
-                        var embeddedText = predictionEngine.Predict(data);
-                        await server.WriteAsync(BitConverter.GetBytes(embeddedText.Vector.Length), stoppingToken);
+                        var embeddedText = GetTextEmbedding(text);
+                        await server.WriteAsync(BitConverter.GetBytes(embeddedText.Length), stoppingToken);
 
-                        var bufferSize = sizeof(float) * embeddedText.Vector.Length;
+                        var bufferSize = sizeof(float) * embeddedText.Length;
                         var buffer = ArrayPool<byte>.Shared.Rent(bufferSize);
-                        Buffer.BlockCopy(embeddedText.Vector, 0, buffer, 0, bufferSize);
+                        Buffer.BlockCopy(embeddedText, 0, buffer, 0, bufferSize);
                         await server.WriteAsync(buffer, 0, bufferSize, stoppingToken);
                         ArrayPool<byte>.Shared.Return(buffer);
 
@@ -76,15 +69,62 @@ namespace EmbeddingsService
                 }
             }
         }
-    }
 
-    class TextData
-    {
-        public string Text { get; set; }
-    }
+        private float[] GetTextEmbedding(string text)
+        {
+            var (inputIds, attentionMask, tokenTypeIds) = tokenizer.Encode(text);
+            attentionMask = PadOrTruncate(attentionMask);
+            var inputIdsTensor = new DenseTensor<long>(PadOrTruncate(inputIds), [1, 128]);
+            var attentionMaskTensor = new DenseTensor<long>(attentionMask, [1, 128]);
+            var tokenTypeIdsTensor = new DenseTensor<long>(PadOrTruncate(tokenTypeIds), [1, 128]);
+            var inputs = new List<NamedOnnxValue>
+                        {
+                            NamedOnnxValue.CreateFromTensor("input_ids", inputIdsTensor),
+                            NamedOnnxValue.CreateFromTensor("attention_mask", attentionMaskTensor),
+                            NamedOnnxValue.CreateFromTensor("token_type_ids", tokenTypeIdsTensor)
+                        };
+            using var results = session.Run(inputs);
 
-    class TransformedTextData : TextData
-    {
-        public float[] Vector { get; set; }
+
+
+            var result = results.First().AsTensor<float>();
+            int seqLen = result.Dimensions[1];
+            int hiddenSize = result.Dimensions[2];
+
+            return MeanPool(result, seqLen, hiddenSize, attentionMask.Span);
+        }
+
+        private Memory<long> PadOrTruncate(Memory<long> input)
+        {
+            if (input.Length > 128)
+                return input.Slice(0, 128);
+
+            else if (input.Length < 128)
+            {
+                var memory = (new long[128]).AsMemory();
+                input.CopyTo(memory);
+                return memory;
+            }
+
+            return input;
+        }
+
+        private static float[] MeanPool(Tensor<float> tensor, int seqLen, int hiddenSize, ReadOnlySpan<long> attentionMask)
+        {
+            float[] meanEmbedding = new float[hiddenSize];
+            int realTokenCount = 0;
+            for (int t = 0; t < seqLen; t++)
+            {
+                if (attentionMask[t] == 1)
+                {
+                    for (int h = 0; h < hiddenSize; h++)
+                        meanEmbedding[h] += tensor[0, t, h];
+                    realTokenCount++;
+                }
+            }
+            for (int h = 0; h < hiddenSize; h++)
+                meanEmbedding[h] /= realTokenCount;
+            return meanEmbedding;
+        }
     }
 }
